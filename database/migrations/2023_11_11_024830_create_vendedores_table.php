0a633d0421de8283de04d34a3e56631f2366e85b
Gabriel <GabrielFelipeS@users.noreply.github.com>
2023-11-11 00:15:48 -0300

    feat: Modificando migration create_vendedores_table

--- database/migrations/2023_11_11_024830_create_vendedores_table.php
+++ database/migrations/2023_11_11_024830_create_vendedores_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('vendedores', function (Blueprint $table) {
             $table->id('codigo_vendedor');
+            $table->string('cpf', 13)->unique();
+            $table->string('nomeCompleto', 100);
+            $table->date('data_de_nascimento');
+            $table->string('nacionalidade', 100);
             $table->timestamps();
         });
     }
