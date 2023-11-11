caf36c78f05ad8d1ca0b514c24942c4c8a34b7d4
Gabriel <GabrielFelipeS@users.noreply.github.com>
2023-11-11 01:21:30 -0300

    feat: UsuarioSeeder pronto

--- database/seeders/EnderecoSeeder.php
+++ database/seeders/EnderecoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Endereco;
 
 class EnderecoSeeder extends Seeder
 {
@@ -12,6 +13,37 @@ class EnderecoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Inserção 1
+        Endereco::create([
+            'cpfDono' => '1234567891233',
+            'bairro' => 'Pimentas',
+            'endereco' => 'Av. Salgado FIlho',
+            'cidade' => 'Guarulhos',
+            'estado' => 'São Paulo',
+            'cep' => '07115-0001',
+            'complemento' => 'Dani Salgados',
+        ]);
+
+        // Inserção 2
+        Endereco::create([
+            'cpfDono' => '123456789124',
+            'bairro' => 'Pimentas',
+            'endereco' => 'Av. Salgado FIlho',
+            'cidade' => 'Guarulhos',
+            'estado' => 'São Paulo',
+            'cep' => '07115-0001',
+            'complemento' => 'Dani Salgados',
+        ]);
+
+        // Inserção 3
+        Endereco::create([
+            'cpfDono' => '12465887',
+            'bairro' => 'Pimentas',
+            'endereco' => 'Av. Salgado FIlho',
+            'cidade' => 'Guarulhos',
+            'estado' => 'São Paulo',
+            'cep' => '07115-000',
+            'complemento' => 'Dani Salgados',
+        ]);
     }
 }
--- database/seeders/UsuariosSeeder.php
+++ database/seeders/UsuariosSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Usuario; 
 
 class UsuariosSeeder extends Seeder
 {
@@ -12,6 +13,23 @@ class UsuariosSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Usuario::create([
+            'nome' => 'Administrador',
+            'nascimento' => '2023-04-14',
+            'telefone' => '(11) 2484-515',
+            'email' => 'admin@admin.com',
+            'senha' => '$2y$10$AL2JJvjD0dqPojG0B/jGGei8Rq851T0NhaMTSipXXsq8nFlYELBBa', // Senha criptografada
+            'fotoPerfil' => 'fotosPerfil/Administrador.png',
+        ]);
+        
+        // Inserção 2
+        Usuario::create([
+            'nome' => 'Teste Testando',
+            'nascimento' => '2023-04-14',
+            'telefone' => '(11) 2484-515',
+            'email' => 'teste@teste.com',
+            'senha' => '$2y$10$QgbyE/MweK0/HgwlVI5mqOc3R90KmsXXyf/qfiRFgYkzrqhfnPnci', // Senha criptografada
+            'fotoPerfil' => 'fotosPerfil/Teste Testando.png',
+        ]);
     }
 }
