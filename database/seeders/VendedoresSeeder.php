32921076153c507fc341a5d78f2905e7344d88f7
Gabriel <GabrielFelipeS@users.noreply.github.com>
2023-11-11 01:15:08 -0300

    feat: VendedoresSeeder pronto

--- database/seeders/VendedoresSeeder.php
+++ database/seeders/VendedoresSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Vendedor;
 
 class VendedoresSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class VendedoresSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Inserção 1
+        Vendedor::create([
+            'codigo_vendedor' => 1,
+            'cpf' => '1234567891233',
+            'nomeCompleto' => 'Gabriel Felipe',
+            'data_de_nascimento' => '2003-04-14',
+            'nacionalidade' => 'Brasileiro',
+        ]);
+
+        // Inserção 2
+        Vendedor::create([
+            'codigo_vendedor' => 2,
+            'cpf' => '123456789124',
+            'nomeCompleto' => 'Gabriely Cavalcante',
+            'data_de_nascimento' => '2003-04-14',
+            'nacionalidade' => 'Brasileiro',
+        ]);
+
+        // Inserção 3
+        Vendedor::create([
+            'codigo_vendedor' => 3,
+            'cpf' => '12465887',
+            'nomeCompleto' => 'Antônio Morrendo das Dores',
+            'data_de_nascimento' => '1996-07-14',
+            'nacionalidade' => 'Brasileiro',
+        ]);
     }
 }
